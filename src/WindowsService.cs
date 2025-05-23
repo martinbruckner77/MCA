c050c5fb1c3fc841447c4f5af3f0fbe46cb1019a
martinbruckner77 <143409190+martinbruckner77@users.noreply.github.com>
2025-05-23 11:54:21 +0200

    Updated MainForm.cs

--- src/WindowsService.cs
+++ src/WindowsService.cs
@@ -29,41 +29,10 @@ namespace MeshAssistant
     public class MeshService : ServiceBase
     {
         private MainForm mainForm;
-        private System.IO.Pipes.NamedPipeServerStream pipeServer;
-        private bool stopRequested = false;
         
         public MeshService()
         {
             ServiceName = "MeshCentralAssistant";
-        }
-
-        private void StartPipeServer()
-        {
-            pipeServer = new System.IO.Pipes.NamedPipeServerStream("MeshAssistantControl", System.IO.Pipes.PipeDirection.In);
-            pipeServer.BeginWaitForConnection(new AsyncCallback(PipeConnectionCallback), null);
-        }
-
-        private void PipeConnectionCallback(IAsyncResult ar)
-        {
-            try
-            {
-                pipeServer.EndWaitForConnection(ar);
-                var reader = new System.IO.StreamReader(pipeServer);
-                string command = reader.ReadLine();
-                
-                if (command == "STOP_AGENT")
-                {
-                    System.Diagnostics.Process[] procs = System.Diagnostics.Process.GetProcessesByName("MeshAgent");
-                    foreach (System.Diagnostics.Process proc in procs)
-                    {
-                        try { proc.Kill(); } catch { }
-                    }
-                }
-                
-                pipeServer.Dispose();
-                if (!stopRequested) { StartPipeServer(); }
-            }
-            catch { }
         }
 
         protected override void OnStart(string[] args)
@@ -72,17 +41,10 @@ namespace MeshAssistant
             System.Windows.Forms.Application.EnableVisualStyles();
             System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
             mainForm = new MainForm(args);
-            StartPipeServer();
         }
 
         protected override void OnStop()
         {
-            stopRequested = true;
-            if (pipeServer != null)
-            {
-                pipeServer.Dispose();
-                pipeServer = null;
-            }
             if (mainForm != null)
             {
                 mainForm.Dispose();
