ed2d9bb7903d31d7276f7da0502550436121a381
martinbruckner77 <143409190+martinbruckner77@users.noreply.github.com>
2025-05-23 11:54:52 +0200

    Updated WindowsService.cs

--- src/WindowsService.cs
+++ src/WindowsService.cs
@@ -29,14 +29,56 @@ namespace MeshAssistant
     public class MeshService : ServiceBase
     {
         private MainForm mainForm;
+        private System.IO.Pipes.NamedPipeServerStream pipeServer;
+        private bool stopRequested = false;
         
         public MeshService()
         {
             ServiceName = "MeshCentralAssistant";
         }
 
+        private void StartPipeServer()
+        {
+            try
+            {
+                pipeServer = new System.IO.Pipes.NamedPipeServerStream("MeshAssistantControl", System.IO.Pipes.PipeDirection.In);
+                pipeServer.BeginWaitForConnection(new AsyncCallback(PipeConnectionCallback), null);
+            }
+            catch (Exception) { }
+        }
+
+        private void PipeConnectionCallback(IAsyncResult ar)
+        {
+            try
+            {
+                pipeServer.EndWaitForConnection(ar);
+                var reader = new System.IO.StreamReader(pipeServer);
+                string command = reader.ReadLine();
+                
+                if (command == "STOP_AGENT")
+                {
+                    // Kill MeshAgent.exe process
+                    foreach (var process in System.Diagnostics.Process.GetProcessesByName("MeshAgent"))
+                    {
+                        try { process.Kill(); } catch (Exception) { }
+                    }
+                }
+                
+                pipeServer.Dispose();
+                if (!stopRequested) { StartPipeServer(); }
+            }
+            catch (Exception) 
+            {
+                try { pipeServer.Dispose(); } catch (Exception) { }
+                if (!stopRequested) { StartPipeServer(); }
+            }
+        }
+
         protected override void OnStart(string[] args)
         {
+            stopRequested = false;
+            StartPipeServer();
+
             // Create main form in service context
             System.Windows.Forms.Application.EnableVisualStyles();
             System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
@@ -45,6 +87,9 @@ namespace MeshAssistant
 
         protected override void OnStop()
         {
+            stopRequested = true;
+            try { pipeServer.Dispose(); } catch (Exception) { }
+
             if (mainForm != null)
             {
                 mainForm.Dispose();
